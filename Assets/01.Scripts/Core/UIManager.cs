79d0f54b1cecb539b9fd2e02a879e58bb6670c5d
VsPot07 <cocoacake84@gmail.com>
2023-07-23 19:01:05 +0900

    0723

--- Assets/01.Scripts/Core/UIManager.cs
+++ Assets/01.Scripts/Core/UIManager.cs
@@ -8,6 +8,10 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
     [SerializeField] private TextMeshProUGUI _guideTxt;
+    private RectTransform _guideTxtRect;
+    [SerializeField] private Canvas _canvas;
+    private RectTransform _canvasRect;
+    private Camera _worldCam;
 
     private void Awake()
     {
@@ -19,9 +23,21 @@ public class UIManager : MonoBehaviour
         Instance = this;
     }
 
+    private void Start()
+    {
+        _canvasRect = _canvas.GetComponent<RectTransform>();
+        _guideTxtRect = _guideTxt.GetComponent<RectTransform>();
+        _worldCam = _canvas.worldCamera;
+    }
+
+    Vector3 setPos;
     public void SetGuideText(string txtInfo, Vector2 pos)
     {
         _guideTxt.SetText(txtInfo);
-        _guideTxt.transform.localPosition = GameManager.Instance.MainCam.ScreenToWorldPoint(pos);
+        setPos = GameManager.Instance.MainCam.WorldToScreenPoint(pos + new Vector2(0, 2));
+        var localPos = new Vector2(0, 20);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvasRect, setPos, _worldCam, out localPos);
+
+        _guideTxtRect.localPosition = localPos;
     }
 }
