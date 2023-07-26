8ce4d247ed127058bcd87d98434a865bf60e9793
VsPot07 <cocoacake84@gmail.com>
2023-07-26 18:26:18 +0900

    0726

---
+++ Assets/01.Scripts/Item/ItemBase.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : PoolableMono
+{
+    public void PupItem(Vector3 targetPos)
+    {
+
+    }
+
+    public override void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+}
